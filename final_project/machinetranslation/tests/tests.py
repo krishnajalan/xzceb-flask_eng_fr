ac13d0d2217aed13d1a38c0138df80ef132902d9
krishna jalan <krishnajalan2001@gmail.com>
2022-07-17 17:30:56 +0000

    Add Translator Script and Support to Server

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from final_project.machinetranslation.translator import english_to_french, french_to_english
+
+class TestEnglishToFrench(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+class TestFrenchToEnglish(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+
+if __name__ == "__main__":
+    unittest.main()
